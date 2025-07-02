a7fc1b5fb869762471ddb9a05d7b8ae2c0f2ee36
RCDev <rcarrillorosario@gmail.com>
2025-07-02 00:47:05 -0700

    Create an agent that acts as a pokedex using the pokeapi API + gemini

---
+++ pokedex_agent/pokedex.py
@@ -0,0 +1,58 @@
+from langchain_core.tools import tool
+from langgraph.prebuilt import create_react_agent
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+POKE_API_URL = "https://pokeapi.co/api/v2/pokemon"
+FLAVORS_URL = "https://pokeapi.co/api/v2/pokemon-species"
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=GOOGLE_API_KEY)
+
+@tool
+def get_pokemon_data (pokemon:str) -> str:
+    """
+    Gets the information of the pokemon from the parameter in json format, including name, description, id, attacks
+
+    Args:
+        pokemon: Name of the pokemon to get information from
+    """
+    response = requests.get(f'{POKE_API_URL}/{pokemon}')
+    return response.json()
+
+@tool
+def get_pokemon_description(pokemon_id:int) -> str:
+    """
+    Get's the pokemon description for the pokemon_id on different languages
+
+    Args:
+        pokemon_id: id of the pokemon to get description from
+    """
+
+    response = requests.get(f'{FLAVORS_URL}/{pokemon_id}')
+    return response.json()
+
+agent = create_react_agent(
+    model=llm,
+    tools=[get_pokemon_data,get_pokemon_description],
+    prompt=(
+        "You're a helpful assistant that will return any json data on a human readable format"
+        "If the user needs the pokemon's description then get the pokemon data first, then"
+        "use the id from the json to get the pokemon description in the language of the original prompt" 
+        "Avoid returning links in the response by just ignoring that part of the prompt"
+    )
+)
+
+res = agent.invoke(
+    {"messages": [{"role": "user", "content": "what pokemon is pichu? give me its description and locations"}]}
+)
+
+for msg in reversed(res["messages"]):
+    if msg.__class__.__name__ == "AIMessage" and msg.content:
+        print("AI:", msg.content)
+        break
